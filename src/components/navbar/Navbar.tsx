5533d4648d3c66a2c7079496556a6cb28478c658
KingRayhan <rayhan095@gmail.com>
2023-04-06 00:26:01 +0600

    integrate mantine

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,18 +6,16 @@ import NavbarAction from "./NavbarAction";
 
 const Navbar = () => {
   return (
-    <div>
-      {" "}
-      <div className="navbar app-border-color">
-        <div className="navbar__inner wrapper">
-          <div className="navbar__left">
-            <Link href="/" className="inline-block">
-              <DarkLogo className="navbar__logo" />
-            </Link>
-          </div>
-          <SearchInput />
-          <NavbarAction />
+    <div className="navbar app-border-color">
+      <div className="navbar__inner wrapper">
+        <div className="navbar__left">
+          <Link href="/">
+            <DarkLogo className={"navbar__logo"} />
+          </Link>
         </div>
+
+        <SearchInput />
+        <NavbarAction />
       </div>
     </div>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,28 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
 
 import { api } from "@/utils/api";
 
-import "@/styles/globals.scss";
+import "@/styles/app.scss";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: "light",
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 
   //  return (
   //    <SessionProvider session={session}>
