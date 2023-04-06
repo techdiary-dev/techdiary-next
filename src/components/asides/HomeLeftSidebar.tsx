a7efb0ea1bf6de1df4d686536eb46044353c4f57
NishatFariza <nishatfarzia78@gmail.com>
2023-04-06 12:26:28 +0600

    left side bar top

--- src/components/asides/HomeLeftSidebar.tsx
+++ src/components/asides/HomeLeftSidebar.tsx
@@ -2,6 +2,7 @@ import { Title } from "@mantine/core";
 import Image from "next/image";
 import React from "react";
 import { AiOutlineHome } from "react-icons/ai";
+import { HiOutlineBookmark, HiPlus } from "react-icons/hi";
 
 const tags = [
   {
@@ -101,10 +102,18 @@ const tags = [
 const HomeLeftSidebar = () => {
   return (
     <div className="flex flex-col gap-3 ">
-      <div className="my-4">
-        <p className="flex ">
-          <AiOutlineHome />
-          <span>হোম</span>
+      <div className="mt-4 mb-2 flex flex-col gap-4">
+        <p className="flex items-center gap-2">
+          <AiOutlineHome color="#1e293b" />
+          <span className="text-sm text-[#1e293b]">হোম</span>
+        </p>
+        <p className="flex items-center gap-2">
+          <HiOutlineBookmark color="#1e293b" />
+          <span className="text-sm text-[#1e293b]">রিডিং লিস্ট</span>
+        </p>
+        <p className="flex items-center gap-2">
+          <HiPlus color="#1e293b" />
+          <span className="text-sm text-[#1e293b]">নতুন ডায়েরি</span>
         </p>
       </div>
       <h3 className="text-sm font-semibold text-gray-600 dark:text-slate-300">
